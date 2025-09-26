bc370c2345dc1ba8f19ff3a71979de7de05facd0
Aleksei Nalivaiko <aleks@MacBook-Pro-Aleksei.local>
2025-09-26 18:40:51 +0300

    Add built site for GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  distDir: "docs",
+  images: {
+    unoptimized: true,
+  },
+  basePath: isProd ? "/next-portfolio-model" : "",
+  assetPrefix: isProd ? "/next-portfolio-model/" : "",
+  trailingSlash: true, // ← Рекомендую добавить для избежания проблем с путями
 };
 
 export default nextConfig;
